50f6147c1b8d3387631a8220770c9c10b7a5393d
Martijn <mbrummelhuis@gmail.com>
2025-04-08 19:41:19 +0200

    added tactip test launch file

---
+++ src/ats_bringup/launch/tactip_only.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+"""
+Launch file for testing the TacTip ROS2 driver. 
+
+The package can be launched with 'ros2 launch ats_bringup test_tactip.launch.py'
+"""
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    
+    tactip_ros2_node = Node(
+        package='tactip_ros2_driver',
+        executable='tactip_ros2_driver',
+        name='tactip_ros2_driver',
+        output='screen',
+        parameters=[
+            {'source': 4},
+            {'frequency': 25.},
+            {'verbose': False},
+            {'test_model_time': True}
+        ],
+    )
+    
+    ld.add_action(tactip_ros2_node)
+    
+    return ld
